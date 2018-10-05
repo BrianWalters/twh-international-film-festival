47c75468834219e7c90844de0ab08555ca79780f
Brian Walters <brianw@pixotech.com>
2018-10-04 22:03:51 -0500

    movie detail page

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -1,90 +1,22 @@
 <?php
 
+
 namespace App\Controller;
 
 use App\Entity\Movie;
-use App\Form\MovieType;
-use App\Repository\MovieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * @Route("/movie-admin")
- */
+
 class MovieController extends AbstractController
 {
     /**
-     * @Route("/", name="movie_index", methods="GET")
+     * @Route("/movie/{movie}", name="movie_details")
      */
-    public function index(MovieRepository $movieRepository): Response
+    public function movieDetailsAction(Movie $movie)
     {
-        return $this->render('movie/index.html.twig', ['movies' => $movieRepository->findAll()]);
-    }
-
-    /**
-     * @Route("/new", name="movie_new", methods="GET|POST")
-     */
-    public function new(Request $request): Response
-    {
-        $movie = new Movie();
-        $form = $this->createForm(MovieType::class, $movie);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($movie);
-            $em->flush();
-
-            return $this->redirectToRoute('movie_index');
-        }
-
-        return $this->render('movie/new.html.twig', [
+        return $this->render('movie.html.twig', [
             'movie' => $movie,
-            'form' => $form->createView(),
         ]);
-    }
-
-    /**
-     * @Route("/{id}", name="movie_show", methods="GET")
-     */
-    public function show(Movie $movie): Response
-    {
-        return $this->render('movie/show.html.twig', ['movie' => $movie]);
-    }
-
-    /**
-     * @Route("/{id}/edit", name="movie_edit", methods="GET|POST")
-     */
-    public function edit(Request $request, Movie $movie): Response
-    {
-        $form = $this->createForm(MovieType::class, $movie);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('movie_edit', ['id' => $movie->getId()]);
-        }
-
-        return $this->render('movie/edit.html.twig', [
-            'movie' => $movie,
-            'form' => $form->createView(),
-        ]);
-    }
-
-    /**
-     * @Route("/{id}", name="movie_delete", methods="DELETE")
-     */
-    public function delete(Request $request, Movie $movie): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$movie->getId(), $request->request->get('_token'))) {
-            $em = $this->getDoctrine()->getManager();
-            $em->remove($movie);
-            $em->flush();
-        }
-
-        return $this->redirectToRoute('movie_index');
     }
 }
