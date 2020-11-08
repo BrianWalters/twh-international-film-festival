2ba946e5bc4401b481894bd2c972526702acf881
Brian Walters <brianw@pixotech.com>
2020-11-08 15:46:21 -0600

    Add a footer with links to other feasts

---
+++ src/Service/Footer.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace App\Service;
+
+
+class Footer
+{
+    public function getLinks()
+    {
+        return [
+            [
+                'text' => 'Feast of the Flesh 9',
+                'url' => 'http://feast-of-the-flesh-9.bdubcodes.net/',
+            ],
+            [
+                'text' => 'Feast of the Flesh 10',
+                'url' => 'http://feast-of-the-flesh-10.bdubcodes.net/',
+            ]
+        ];
+    }
+}
